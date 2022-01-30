311ef17a4fc7fa792906bc0fc93facc9c1a7376d
micha <micha@uxsrv.de>
2022-01-30 18:45:29 +0100

    fix: GPL v2

--- _LibreLightDesk.py
+++ _LibreLightDesk.py
@@ -6,7 +6,7 @@ This file is part of LibreLight.
 
 LibreLight is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License.
+the Free Software Foundation, version 2 of the License.
 
 LibreLight is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of

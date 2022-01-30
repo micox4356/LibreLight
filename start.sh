fdb06fc5e6f19958518afd2086169a1a7868d568
micha <micha@librelight.de>
2022-01-30 21:43:57 +0100

    fix: dmx monito on boot with linux:netns (root)

--- start.sh
+++ start.sh
@@ -1,14 +1,21 @@
+screen -ls
 set -e
-killall screen | echo  ""
-killall SCREEN | echo  ""
+screen -XS ASP quit | echo ""
+screen -XS console quit | echo ""
+screen -XS Editor quit | echo ""
+#killall screen | echo  ""
+#killall SCREEN | echo  ""
 path="/opt/LibreLight"
 cd "$path"
 screen -d -m -S ASP sh $path/ASP/start_ASP.sh 
 cd $path/Xdesk/
 screen -d -m -S console python3 $path/Xdesk/console.py 
+sleep 3;
+#xterm -e 'screen -r DMX'
+xfce4-terminal -e 'screen -r DMX' --hide-menubar --zoom=-2 --geometry=90x35+800+520
+sleep 2
 screen -d -m -S Editor python3 $path/Xdesk/LibreLightDesk.py 
 
-sleep 2
 screen -ls
 echo "BOOT END"
-sleep 10;
+sleep 3;

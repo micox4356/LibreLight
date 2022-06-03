84987579744065aaf30ff9e85294d431bebf49a9
micha <micha@uxsrv.de>
2022-06-03 18:24:27 +0200

    version 22.5

--- _LibreLightDesk.py
+++ _LibreLightDesk.py
@@ -20,7 +20,7 @@ along with LibreLight.  If not, see <http://www.gnu.org/licenses/>.
 """
 import random
 rnd_id = str(random.randint(1000,9000))
-rnd_id += " Beta 22.02 "
+rnd_id += " Beta 22.05 "
 import subprocess
 try:
     rnd_id += subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()

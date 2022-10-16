14a2f37182ba7965e0e411813071fe3beb0c2383
micha <micha@librelight.de>
2022-10-16 22:48:18 +0200

    fix pull origin dev

--- dev/pull_dev.sh
+++ dev/pull_dev.sh
@@ -1,5 +1,13 @@
 git branch dev
 git checkout dev
 git branch 
-git pull --set-upstream origin dev
+git pull --set-upstream origin dev --rebase
+
+
+# or
+# git branch dev
+# git switch dev
+# git branch --set-upstream-to=origin/dev dev
+# git pull origin dev --rebase
+
 

4c55176fa9aa16b1e46998c8e0e5c77086c8946c
micha <micha@librelight.de>
2022-11-28 21:53:24 +0100

    add install.sh with debian dependecies and pip install

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/bash
+
+ID=$(id -u)
+if [ "x$ID" != 'x0' ]; then
+    echo "please start with sudo"
+    exit 
+fi
+echo "-- apt update"
+apt update
+#apt upgrade -y
+
+echo ""
+echo "-- system tools"
+pkg="vim lm-sensors htop nmap tcpdump rsync git psmisc screen git gitk" 
+apt install $pkg 
+
+echo ""
+echo "-- touchscreen tools"
+#xautomation -> xte mouse  #xrand -> x11-xserver-utils
+pkg="xdotool x11-xserver-utils xinput xautomation" 
+apt install $pkg 
+
+echo ""
+echo "-- python pkg's"
+pkg="python3-tk python3-pygame memcached python3-memcache python3-pip python3-pyglet "
+apt install $pkg 
+
+echo ""
+echo "-- python 3d pkg's"
+pkg="python3-opengl python3-pyglet python3-pil "
+apt install $pkg 
+#exit
+
+echo ""
+echo "-- python-pip pkg's"
+su -- user <<EOF
+pip install pyopengltk
+pip install moderngl
+pip install moderngl-window
+echo ""
+pip install glfw
+pip install glwindow
+pip install glnext
+#pip install glnext_compiler
+pip install glcontext
+pip3 install pyopengltk
+EOF
+
+echo ""
+echo "-- sync netns"
+rsync -apv /opt/LibreLight/ASP/netns/ /opt/netns/
+chmod -R 755 /opt/netns/ 
+chown -R root:root /opt/netns/
+
+
+
+

90e15ed1db298245d7eae86e897c1d57203d1ea6
micha <micha@librelight.de>
2022-12-08 01:02:32 +0100

    add vpu dir with auto start/stop script's

---
+++ vpu/autostart.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+#insert in xfce4-session on login event 
+set -x
+#firefox-esr -P
+
+screen -m -d -S vnc -- x11vnc -forever
+
+screen -m -d -S ASP -- python3 /opt/LibreLight/ASP/ArtNetProcessor.py
+
+screen -m -d -S shader -- python3 /opt/LibreLight/Xdesk/3d/demo_shader_live.py
+
+sleep 3
+screen -ls

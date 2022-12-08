d98e2fd8d0491f08d17b6f0c00a539c41b6cb9b9
micha <micha@librelight.de>
2022-12-08 01:17:25 +0100

    add scren -XS ses quit

--- vpu/autostart.sh
+++ vpu/autostart.sh
@@ -4,12 +4,16 @@
 set -x
 #firefox-esr -P
 
+#screen -XS "vnc" quit
+screen -XS "ASP" quit
+screen -XS "shader" quit
+
 screen -m -d -S vnc -- x11vnc -forever
 
 screen -m -d -S ASP -- python3 /opt/LibreLight/ASP/ArtNetProcessor.py
 
 #screen -m -d -S shader -- python3 /opt/LibreLight/Xdesk/3d/demo_shader_live.py
-screen -m -d -S shader -- python3 /opt/LibreLight/Xdesk/vpu/demo_shader_live.py
+screen -m -d -S shader -- python3 /opt/LibreLight/Xdesk/vpu/shader_live.py
 
 sleep 3
 screen -ls

e92f139ec232a759f94fd37089f8e41891e1818d
micha <micha@librelight.de>
2022-12-11 19:29:32 +0100

    activate vpu_live.py pygame as vpu

--- vpu/autostart.sh
+++ vpu/autostart.sh
@@ -13,7 +13,8 @@ screen -m -d -S vnc -- x11vnc -forever
 screen -m -d -S ASP -- python3 /opt/LibreLight/ASP/ArtNetProcessor.py
 
 #screen -m -d -S shader -- python3 /opt/LibreLight/Xdesk/3d/demo_shader_live.py
-screen -m -d -S shader -- python3 /opt/LibreLight/Xdesk/vpu/shader_live.py
+#screen -m -d -S shader -- python3 /opt/LibreLight/Xdesk/vpu/shader_live.py
+screen -m -d -S shader -- python3 /opt/LibreLight/Xdesk/vpu/vpu_live.py
 
 sleep 3
 screen -ls

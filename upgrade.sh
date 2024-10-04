fdd339da9ed521e0f2a29c5bac42d0eff946270f
micha <micha@librelight.de>
2024-10-04 20:08:22 +0200

    fix: upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -9,7 +9,8 @@ ls -l
 rm -rf LibreLightASP
 #git clone https://github.com/micox4356/LibreLightASP.git
 git clone https://gogs.librelight.de/librelight/ASP.git
-rsync -apv --delete /opt/LibreLight/git/LibreLightASP/ /opt/LibreLight/ASP/
+#rsync -apv --delete /opt/LibreLight/git/LibreLightASP/ /opt/LibreLight/ASP/
+rsync -apv --delete /opt/LibreLight/git/ASP/ /opt/LibreLight/ASP/
 pwd
 ls -l
 
@@ -19,7 +20,8 @@ pwd
 ls -l
 #git clone https://github.com/micox4356/LibreLight.git
 git clone https://gogs.librelight.de/librelight/Xdesk.git
-rsync -apv --delete /opt/LibreLight/git/LibreLight/ /opt/LibreLight/Xdesk/
+#rsync -apv --delete /opt/LibreLight/git/LibreLight/ /opt/LibreLight/Xdesk/
+rsync -apv --delete /opt/LibreLight/git/Xdesk/ /opt/LibreLight/Xdesk/
 pwd
 ls -l
 read -p -t 1 
